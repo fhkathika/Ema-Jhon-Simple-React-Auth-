49bb8930b9dc77c2cc8a39fe34251a154a629405
fhkathika <fhkathika057@gmail.com>
2021-10-13 20:18:21 +0600

    edited

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import useFirebase from '../hooks/userFirebase';
+import useFirebase from '../hooks/useFirebase';
 
 export const AuthContext=createContext();
 const AuthProvider = ({children}) => {
